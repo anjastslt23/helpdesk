0e0761f700d72877dcc1c05eb043e9b3db26cf52
Anjasta Bagus Tarigan <anjastatarigan02@gmail.com>
2024-03-21 17:54:07 +0800

    Penambahan grouping rute website

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,5 +12,13 @@ $routes->get('/', 'SigninController::index');
 $routes->get('lupa_password', 'SigninController::lupaPassword');
 $routes->post('initialize', 'SigninController::loginRules');
 
+// Group untuk bagian admin
+
+$routes->group('admin', ['filter' => 'authGuard'], function ($routes) {
+
+    $routes->group('', function ($routes) {
+        $routes->get('dashboard', 'Admin\DashboardControl::index');
+    });
+});
 
 $routes->get('admin/dashboard', 'DashboardController::index');
